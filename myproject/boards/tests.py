42d9ebaa61a5788042701b1b7ee27e32bfb2fdd6
trans-bug <2541248912@qq.com>
2022-02-24 22:49:13 +0800

    Changes to be committed: 	new file:   myproject/boards/__pycache__/forms.cpython-310.pyc 	modified:   myproject/boards/__pycache__/tests.cpython-310.pyc 	modified:   myproject/boards/__pycache__/views.cpython-310.pyc 	new file:   myproject/boards/forms.py 	modified:   myproject/boards/tests.py 	modified:   myproject/boards/views.py 	modified:   myproject/db.sqlite3 	modified:   myproject/myproject/__pycache__/settings.cpython-310.pyc 	modified:   myproject/myproject/__pycache__/urls.cpython-310.pyc 	modified:   myproject/myproject/settings.py 	modified:   myproject/myproject/urls.py 	new file:   myproject/static/css/app.css 	new file:   myproject/templates/base.html 	modified:   myproject/templates/home.html 	new file:   myproject/templates/includes/form.html 	new file:   myproject/templates/new_topic.html 	modified:   myproject/templates/topics.html

--- myproject/boards/tests.py
+++ myproject/boards/tests.py
@@ -7,23 +7,33 @@ from django.urls import resolve
 from django.views import View
 #from .views import home
 
-from .views import home, board_topics
-from .models import Board
+from django.contrib.auth.models import User
+from .views import home, board_topics,new_topic
+from .models import Board,Topic,Post
+
+from .forms import NewTopicForm
 
 
 class HomeTests(TestCase):
+    def setUp(self):
+        self.board = Board.objects.create(name='Django', description='Django board.')
+        url = reverse('home')
+        self.response = self.client.get(url)
     def test_home_view_status_code(self):
         url = reverse('home')
         response = self.client.get(url)
         self.assertEquals(response.status_code, 200)
     def test_home_url_resolves_home_view(self):
         View=resolve('/')
-        self.assertEquals(View.func,home)
+        self.assertEquals(View.func,home)#mark,这儿可能出现问题
+    def test_home_view_contains_link_to_topics_page(self):
+        board_topics_url = reverse('board_topics', kwargs={'pk': self.board.pk})
+        self.assertContains(self.response, 'href="{0}"'.format(board_topics_url))
         
 class BoardTopicsTests(TestCase):
-    def setUp(self):
+    def setUp(self):#setup创建一个临时的实例
         Board.objects.create(name='Django', description='Django board.')
-    def test_board_topics_view_success_status_code(self):
+    def test_board_topics_view_success_status_code(self):#将url和response添加到setup
         url = reverse('board_topics', kwargs={'pk': 1})
         response = self.client.get(url)
         self.assertEquals(response.status_code, 200)
@@ -34,3 +44,105 @@ class BoardTopicsTests(TestCase):
     def test_board_topics_url_resolves_board_topics_view(self):
         view = resolve('/boards/1/')
         self.assertEquals(view.func, board_topics)
+    #返回链接测试
+    def test_board_topics_view_contains_link_back_to_homepage(self):
+        board_topics_url = reverse('board_topics', kwargs={'pk': 1})
+        response = self.client.get(board_topics_url)
+        homepage_url = reverse('home')
+        self.assertContains(response, 'href="{0}"'.format(homepage_url))
+    #测试导航链接，包括来回两个链接
+    def test_board_topics_view_contains_navigation_links(self):
+        board_topics_url = reverse('board_topics', kwargs={'pk': 1})
+        homepage_url = reverse('home')
+        new_topic_url = reverse('new_topic', kwargs={'pk': 1})
+        
+        response = self.client.get(board_topics_url)
+        
+        self.assertContains(response, 'href="{0}"'.format(homepage_url))
+        self.assertContains(response, 'href="{0}"'.format(new_topic_url))
+        
+class NewTopicTests(TestCase):
+    def setUp(self):
+        Board.objects.create(name='Django', description='Django board.')
+        User.objects.create_user(username='john', email='john@doe.com', password='123')  # <- included this line here
+
+        
+    def test_new_topic_view_success_status_code(self):
+        url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+        
+    def test_new_topic_view_not_found_status_code(self):
+        url = reverse('new_topic', kwargs={'pk': 99})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 404)
+        
+    def test_new_topic_url_resolves_new_topic_view(self):
+        view = resolve('/boards/1/new/')
+        self.assertEquals(view.func, new_topic)
+        
+    def test_new_topic_view_contains_link_back_to_board_topics_view(self):
+        new_topic_url = reverse('new_topic', kwargs={'pk': 1})
+        board_topics_url = reverse('board_topics', kwargs={'pk': 1})
+        response = self.client.get(new_topic_url)
+        self.assertContains(response, 'href="{0}"'.format(board_topics_url))
+    # setUp：创建一个测试中使用的 Board 实例
+    # test_new_topic_view_success_status_cod：检查发给 view 的请求是否成功
+    # test_new_topic_view_not_found_status_code：检查当 Board 不存在时 view 是否会抛出一个 404 的错误
+    # test_new_topic_url_resolves_new_topic_view：检查是否正在使用正确的 view
+    # test_new_topic_view_contains_link_back_to_board_topics_view：确保导航能回到 topics 的列表
+    def test_csrf(self):
+        url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.get(url)
+        self.assertContains(response, 'csrfmiddlewaretoken')
+    def test_new_topic_valid_post_data(self):
+        url = reverse('new_topic', kwargs={'pk': 1})
+        data = {
+            'subject': 'Test title',
+            'message': 'Lorem ipsum dolor sit amet'
+        }
+        response = self.client.post(url, data)
+        self.assertTrue(Topic.objects.exists())
+        self.assertTrue(Post.objects.exists())
+    def test_new_topic_invalid_post_data(self):
+        '''
+        Invalid post data should not redirect
+        The expected behavior is to show the form again with validation errors
+        '''
+        url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.post(url, data={})
+        self.assertEquals(response.status_code, 200)
+    def test_new_topic_invalid_post_data_empty_fields(self):
+        '''
+        Invalid post data should not redirect
+        The expected behavior is to show the form again with validation errors
+        '''
+        url = reverse('new_topic', kwargs={'pk': 1})
+        data = {
+            'subject': '',
+            'message': ''
+        }
+        response = self.client.post(url, data)
+        self.assertEquals(response.status_code, 200)
+        self.assertFalse(Topic.objects.exists())
+        self.assertFalse(Post.objects.exists())
+    # setUp：包含 User.objects.create_user 以创建用于测试的 User 实例。
+    # test_csrf：由于 CSRF Token 是处理 Post 请求的基本部分，我们需要保证我们的 HTML 包含 token。
+    # test_new_topic_valid_post_data：发送有效的数据并检查视图函数是否创建了 Topic 和 Post 实例。
+    # test_new_topic_invalid_post_data：发送一个空字典来检查应用的行为。
+    # test_new_topic_invalid_post_data_empty_fields：类似于上一个测试，但是这次我们发送一些数据。预期应用程序会验证并且拒绝空的 subject 和 message。
+    def test_contains_form(self):  # <- new test
+        url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.get(url)
+        form = response.context.get('form')
+        self.assertIsInstance(form, NewTopicForm)
+    def test_new_topic_invalid_post_data(self):  # <- updated this one
+        '''
+        Invalid post data should not redirect
+        The expected behavior is to show the form again with validation errors
+        '''
+        url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.post(url, {})
+        form = response.context.get('form')
+        self.assertEquals(response.status_code, 200)
+        self.assertTrue(form.errors)
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -23,5 +23,6 @@ from boards import views
 urlpatterns = [
     re_path(r'^$', views.home, name='home'),
     re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     re_path(r'admin/', admin.site.urls),
 ]
